9fdfe5b1d89564e0ce1212ef6b7f97e5c93b6294
Maverun <Maverun12345@gmail.com>
2016-05-15 21:25:43 -0400

    Ugh should use github for now..

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -1,42 +1,63 @@
 from discord.ext import commands
-from Cogs.Utils import Read,Utils
+from cogs.utils import utils
 import datetime
 import glob
-import Storage
+import storage
 import discord
 import traceback
+import asyncio
+import inspect
+import re
 
+description = '''Nurevam's Command List. '''
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"), description=description,pm_help=False)
+bot.db= storage.Redis()
 
-Config=Read.config
-description = '''{} bot Command List. '''.format(Config["Bot name"])
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"), description=description,pm_help=Config["PM_Help"])
+async def say_edit(msg):
+    key = str(inspect.getmodule(inspect.currentframe().f_back.f_code))
+    regex = re.compile(r"(cogs.[a-zA-Z]*)")
+    get = re.search(regex,key)
+    if get:
+        word = await bot.say(msg)
+        check = await bot.db.redis.hgetall("{}:Config:Delete_MSG".format(word.server.id))
+        if len(check)>0:
+            if check[get.groups()[0][5:]] == "on":
+                await asyncio.sleep(30)
+                await bot.delete_message(word)
+    else:
+        print("NONE")
+    return
+
+bot.says_edit=say_edit
 
 @bot.event
 async def on_ready():
     print('Logged in')
     print(bot.user.id)
     print('------')
-    bot.uptime = datetime.datetime.utcnow()
+    if not hasattr(bot, 'uptime'):
+            bot.uptime = datetime.datetime.utcnow()
+    utils.redis_connection()
     load_cogs()
-    await Storage.Redis().Save(False)
 
-def get_bot_uptime():
-    now = datetime.datetime.utcnow()
-    delta = now - bot.uptime
-    hours, remainder = divmod(int(delta.total_seconds()), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    days, hours = divmod(hours, 24)
-    if days:
-        fmt = '**{d}** days, **{h}** hours, **{m}** minutes, and **{s}** seconds'
-    else:
-        fmt = '**{h}** hours, **{m}** minutes, and **{s}** seconds'
+@bot.event
+async def on_message(msg): #For help commands.
+    try:
+        cmd_prefix= (await bot.db.redis.get("{}:Config:CMD_Prefix".format(msg.server.id)))
+        cmd_prefix=cmd_prefix.split(",")
+        if '' in cmd_prefix: #check if "none-space" as a command, if true, return, in order to prevert any spam in case, lower chance of getting kick heh.
+            return
+        bot.command_prefix = commands.when_mentioned_or(*cmd_prefix)
+        if "help" in msg.content:
+            if await bot.db.redis.get("{}:Config:Whisper".format(msg.server.id)) == "on":
+                bot.pm_help =True
+            else:
+                bot.pm_help=False
+    except:
+        pass
 
-    return fmt.format(d=days, h=hours, m=minutes, s=seconds)
+    await bot.process_commands(msg)
 
-@bot.command()
-async def uptime(): #Showing Time that bot been total run
-    """Tells you how long the bot has been up for."""
-    await bot.say("I've been up for {}".format(get_bot_uptime()))
 
 def load_cogs():
     cogs = list_cogs()
@@ -45,18 +66,19 @@ def load_cogs():
             bot.load_extension(cogs)
             print ("Load {}".format(cogs))
         except Exception as e:
-            Utils.prRed(cogs)
-            print(e)
-            raise
+            utils.prRed(cogs)
+            utils.prRed(e)
+            # continue
+            # raise
 
 def list_cogs():
-    cogs = glob.glob("Cogs/*.py")
+    cogs = glob.glob("cogs/*.py")
     clean = []
     for c in cogs:
         c = c.replace("/", "\\") # Linux fix
         if "__init__" in c:
             continue
-        clean.append("Cogs." + c.split("\\")[1].replace(".py", ""))
+        clean.append("cogs." + c.split("\\")[1].replace(".py", ""))
     return clean
 
 async def send_cmd_help(ctx):
@@ -72,27 +94,29 @@ async def send_cmd_help(ctx):
 @bot.event
 async def on_command_error(error,ctx):
     print(error)
-    print(ctx.message.content)
+    print(ctx.message.clean_content)
     if isinstance(error, commands.MissingRequiredArgument):
         await send_cmd_help(ctx)
     elif isinstance(error,commands.BadArgument):
         await send_cmd_help(ctx)
 
 @bot.event
-async def on_command(command,ctx):
-    # print(ctx.message.content)
-    pass
-
-@bot.event
 async def on_error(event,*args,**kwargs):
-    Utils.prRed("Error!")
     Current_Time = datetime.datetime.utcnow().strftime("%b/%d/%Y %H:%M:%S UTC")
-    Utils.prGreen(Current_Time)
-    Utils.prRed(traceback.format_exc())
+    utils.prYellow(*args)
+    print(**kwargs)
+    utils.prRed(Current_Time)
+    utils.prRed("Error!")
+    utils.prRed(traceback.format_exc())
     error =  '```py\n{}\n```'.format(traceback.format_exc())
     await bot.send_message(bot.get_channel("123934679618289669"), "```py\n{}```".format(Current_Time + "\n"+ "ERROR!") + "\n" +  error)
 
+# @bot.command(name="test")
+# async def test():
+#     api = OsuApi("aa79ee8dd3dd29757b848085063f4447947e5328", connector=aiohttp)
+#     results = await api.get_user("peppy")
+#     await bot.say("PP rank is {0}".format(results[0].pp_rank))
 
 if __name__ == '__main__':
-    bot.run(Config['password'])
+    bot.run(utils.OS_Get("NUREVAM_TOKEN"))
 
--- Bot/Cogs/Utils/Utils.py
+++ Bot/Cogs/Utils/Utils.py
@@ -1,11 +1,59 @@
-
+import redis
+import os
 #############Color for Terminal###############################
-def prRed(prt): print("\033[91m {}\033[00m" .format(prt))
-def prGreen(prt): print("\033[92m {}\033[00m" .format(prt))
-def prYellow(prt): print("\033[93m {}\033[00m" .format(prt))
-def prLightPurple(prt): print("\033[94m {}\033[00m" .format(prt))
-def prPurple(prt): print("\033[95m {}\033[00m" .format(prt))
-def prCyan(prt): print("\033[96m {}\033[00m" .format(prt))
-def prLightGray(prt): print("\033[97m {}\033[00m" .format(prt))
-def prBlack(prt): print("\033[98m {}\033[00m" .format(prt))
-##############Easy to debug####################################
+#Whole line, and easier to debug
+def prRed(prt): print("\033[91m{}\033[00m".format(prt))
+def prGreen(prt): print("\033[92m{}\033[00m".format(prt))
+def prYellow(prt): print("\033[93m{}\033[00m".format(prt))
+def prLightPurple(prt): print("\033[94m {}\033[00m".format(prt))
+def prPurple(prt): print("\033[95m{}\033[00m".format(prt))
+def prCyan(prt): print("\033[96m{}\033[00m".format(prt))
+def prLightGray(prt): print("\033[97m{}\033[00m".format(prt))
+def prBlack(prt): print("\033[98m{}\033[00m".format(prt))
+###############################################################
+
+def OS_Get(name):
+    return os.environ.get(name)
+
+#########################################
+#    _____               _   _          #
+#   |  __ \             | | (_)         #
+#   | |__) |   ___    __| |  _   ___    #
+#   |  _  /   / _ \  / _` | | | / __|   #
+#   | | \ \  |  __/ | (_| | | | \__ \   #
+#   |_|  \_\  \___|  \__,_| |_| |___/   #
+#                                       #
+#########################################
+
+###########Connection Line####################
+def redis_connection():
+    global redis
+    redis = redis.Redis(host=OS_Get("Redis"),decode_responses=True)
+
+############Checking if cogs for that server is enable or disable##########
+def is_enable(msg,Cogs):
+    data = redis
+    try:
+        check = data.hget("{}:Config:Cogs".format(msg.message.server.id),Cogs)
+        if check == "on":
+            return True
+        else:
+            return False
+    except:
+        return False
+    pass
+
+######################Checking if Role is able######################################
+def check_roles(msg,Cogs,Roles):
+    data = redis
+    try:
+        Roles= data.hgetall("{}:{}:Config".format(msg.message.server.id,Cogs))
+        checking=msg.message.author.roles
+        for name in checking:
+            if str(name) == Roles:
+                return True
+    except:
+        return False
+#####################Checking if it cooldown#####################################
+def is_cooldown(msg):
+    return redis
