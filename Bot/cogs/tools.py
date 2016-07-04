7e34682c14159ba8e316951b51f60d74a0f9e3c9
Maverun <Maverun12345@gmail.com>
2016-07-04 17:46:15 -0400

    fixed acivity cuz damnit people with comma name

--- Bot/cogs/tools.py
+++ Bot/cogs/tools.py
@@ -225,7 +225,7 @@ class Tools():
         msg=[]
         print(player_data)
         for x in range(0,len(player_data),3):
-            msg.append("{},{},{}\n".format(player_data[x],player_data[x+2],player_data[x+1]))
+            msg.append("{},{},{}\n".format(player_data[x],player_data[x+2].replace(","," "),player_data[x+1]))
         with open("acivity.txt","w",encoding='utf-8') as f:
             for x in msg:
                 print(x)
