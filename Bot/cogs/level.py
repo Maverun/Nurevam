bca5ce8e1b990442e45ee47015db87e3288156a8
Maverun <Maverun12345@gmail.com>
2016-11-29 23:54:19 -0500

    Minor fix with int on None, Level

--- Bot/cogs/level.py
+++ Bot/cogs/level.py
@@ -94,9 +94,10 @@ class Level:
             current_xp = await self.redis.hincrby(self.name,"XP",increment = xp)
             total_xp = await self.redis.hincrby(self.name,"Total_XP",increment = xp)
             await self.redis.hincrby(self.name,"Message Count",increment = 1)
-            next_xp = int(await self.redis.hget(self.name,"Next_XP"))
+            next_xp = await self.redis.hget(self.name,"Next_XP")
             if next_xp == None: #Some reason i get error that Next XP is missing, so best to this way to stop giving error while setting it
                 next_xp = await self.redis.hset(self.name,"Next_XP",100)
+            next_xp = int(next_xp)
             if current_xp >= next_xp:
                 remain_xp = current_xp - next_xp
                 level,new_xp = self.next_Level(total_xp)
