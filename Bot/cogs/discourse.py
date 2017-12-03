e542c65eaf0a2e4af81760f193d3c9193f6ccb69
Maverun <Maverun12345@gmail.com>
2017-12-03 00:01:41 -0500

    This happen when you been doing java for a long time..

--- Bot/cogs/discourse.py
+++ Bot/cogs/discourse.py
@@ -126,7 +126,7 @@ class Discourse(): #Discourse, a forums types.
         status,link,get_post = "???"
         while True:
             counter = await self.redis.incr("{}:Discourse:ID".format(guild_id))
-            print("counter is " + counter)
+            print("counter is ",counter)
             log.debug("Counter is {}".format(counter))
             self.logging_info(get_post, link, counter, guild_id)
             counter += 1
