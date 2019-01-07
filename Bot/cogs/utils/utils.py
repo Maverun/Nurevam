510c06612ffe0ce708579baf9c60f75b7be1a3da
Maverun <Maverun12345@gmail.com>
2019-01-07 02:37:59 -0500

    Minor fix

--- Bot/cogs/utils/utils.py
+++ Bot/cogs/utils/utils.py
@@ -10,7 +10,7 @@ import json
 def prRed(prt): print("\033[91m{}\033[00m".format(prt))
 def prGreen(prt): print("\033[92m{}\033[00m".format(prt))
 def prYellow(prt): print("\033[93m{}\033[00m".format(prt))
-def prLightPurple(prt): print("\033[94m {}\033[00m".format(prt))
+def prLightPurple(prt): print("\033[94m{}\033[00m".format(prt))
 def prPurple(prt): print("\033[95m{}\033[00m".format(prt))
 def prCyan(prt): print("\033[96m{}\033[00m".format(prt))
 def prLightGray(prt): print("\033[97m{}\033[00m".format(prt))
