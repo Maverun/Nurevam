b499d94c76df2960e79b05e3f6cb6f4b8a2e8eba
Maverun <maverun@protonmail.ch>
2021-01-07 23:29:49 -0500

    Add Member Intent.

--- Bot/bot.py
+++ Bot/bot.py
@@ -13,10 +13,15 @@ description = '''Nurevam's Command List.
  To enable more commands, you must visit dashboard to enable certain plugins you want to run.
  If there is a problem with the prefix etc, please do @nurevam prefix to see what prefix you can do
  Any problem relating to Nurevam, please do contact owner Maverun (´･ω･`)#3333
- 
+
  First └ mean it is commands under that plugin, and if there is one or more under commands, it is a sub command that can invoke by doing !parent subcommand such as !rank global
  '''
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description=description,hidden = True,pm_help = False,help_command=helpformat.Custom_format())
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description=description,hidden = True,
+                   pm_help = False,help_command=helpformat.Custom_format(),
+                   intents=intents)
 bot.db= storage.Redis()
 redis = utils.redis
 
@@ -43,6 +48,7 @@ async def on_ready():
         bot.owner = (await bot.application_info()).owner
         bot.background = {}
         bot.id_discourse = 0
+        # bot.eng_lang = utils.read_lang("English")
         load_cogs()
     await bot.change_presence(activity = discord.Game("http://nurevam.site/"))
 
@@ -113,6 +119,7 @@ async def on_error(event,*args,**kwargs):
     error =  '```py\n{}\n```'.format(traceback.format_exc())
     try:
         await bot.owner.send("```py\n{}```".format(Current_Time + "\n"+ "ERROR!") + "\n" + error)
+        # await bot.owner.send(f"```py\n{Current_Time}\nERROR!\n{error}```")
     except:
         utils.prRed("Unable to send to owner!")
 
